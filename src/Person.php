14d1f4761362100aa3b058560ffedb45cc505356
greeflas <vldmr.kuprienko@gmail.com>
2020-08-06 20:53:22 +0300

    Example of method with business logic

--- src/Person.php
+++ src/Person.php
@@ -3,14 +3,33 @@
 class Person
 {
     private $firstName;
+    private $lastName;
 
     public function __construct($firstName)
     {
         $this->firstName = $firstName;
     }
 
+    public function changeName($newFirstName, $newLastName)
+    {
+        $this->validNewName($newFirstName, $newLastName);
+
+        $this->firstName = $newFirstName;
+        $this->lastName = $newLastName;
+    }
+
     public function displayFirstName()
     {
         echo $this->firstName . \PHP_EOL;
+    }
+
+    private function validNewName($firstName, $lastName)
+    {
+        if (
+            $this->firstName === $firstName
+            && $this->lastName === $lastName
+        ) {
+            echo 'Don\'t waste my time!';
+        }
     }
 }
